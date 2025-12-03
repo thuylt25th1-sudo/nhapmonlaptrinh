9b6bcb9bce7548638e215883f561506261fcd65e
Administrator <Administrator@PMT2009>
2025-12-03 14:44:51 +0700

    hoan thanh buoi 3

--- NhapMonLapTrinh/Buoi3/Buoi3.cpp
+++ NhapMonLapTrinh/Buoi3/Buoi3.cpp
@@ -1,1 +1,23 @@
 #include <iostream>
+using namespace std;
+void cau1() {
+	const float PI = 3.14;
+	//Viet chuong trinh dien tich va chu vi hinh tron voi ban kinh r
+	int r;
+	cout << "nhap ban kinh r:";
+	cin >> r;
+	cout << "dien tich:" << PI * r * r << endl;
+	cout << "chu vi: " << 2 * PI * r << endl;
+}
+void cau2() {
+	int luong, phucap;
+	cout << "nhap luong:";
+	cin >> luong;
+	cout << "nhap phu cap:";
+	cin >> phucap;
+	cout << "thuc lanh : " << (luong + phucap) * 0.9 << endl;
+}
+int main()
+{
+	cau2();
+}
