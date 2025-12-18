cc45833ebe9813e6aa4bef1ec9d88ebf57e5a74d
Administrator <Administrator@PMT2010>
2025-12-18 15:44:04 +0700

    hoan thanh buoi 12

---
+++ NhapMonLapTrinh/Buoi12/Buoi12.cpp
@@ -0,0 +1,14 @@
+#include "Array.h"
+#define MAX 100
+
+int main()
+{
+	int a[MAX], n = 0;
+	input(a, n);
+	/*output(a, n);
+	giatrichan(a, n);
+	/*vitriam(a, n);*/
+	/*giatrilonnhat(a, n);*/
+	/*vitrinhonhat(a, n);*/
+	nguyentonhohon100(a, n);
+}
